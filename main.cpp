fa13b7da240bbc9f5f85aa8ea82df068a975d205
Apostoln <nikbond97@gmail.com>
2017-06-19 16:57:48 +0300

    Comments&small fixes are added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-#include <asio.hpp>
 #include <SipServer.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
     SipServer server;
+    if (argc > 1) {
+        //Server port is 0 on default if other value is not specified on main function arguments.
+        //If port value is 0, system set for socket any free and allowed port.
+        auto port = (unsigned short) std::atoi(argv[1]);
+        server.setPort(port);
+    }
     server.run();
     return 0;
 }
--- src/SipServer.hpp
+++ src/SipServer.hpp
@@ -6,15 +6,19 @@
 
 class SipServer {
     private:
-        asio::io_service* serverIo;
+        asio::io_service* serverIo; //Object for connecting to I/O services of OS.
         asio::ip::udp::socket* serverSocket;
         unsigned short port;
     public:
         SipServer(asio::io_service* ioService, unsigned short port) {
+            //Server port is 0 on default if other value is not specified on constructor arguments.
+            //If server port value is 0, system set for new socket any free and allowed udp port.
             this->serverIo = ioService;
             this->port = port;
             this->serverSocket = new asio::ip::udp::socket(*serverIo,
                                                            asio::ip::udp::endpoint(asio::ip::udp::v4(), port));
+
+            //If port is not specified by user, we should find a port number that actually assigned to socket;
             if(port == 0) {
                 this->port = serverSocket->local_endpoint().port();
             }
@@ -37,12 +41,19 @@ class SipServer {
             return port;
         }
 
+        void setPort(unsigned short port) {
+            this->port=port;
+        }
+
         void run() {
             asio::ip::udp::endpoint clientEndPoint;
+
             std::cout << "Server is started" << std::endl
-                      << "Listening udp port " << this->getPort() << std::endl;
+                      << "Listening UDP port " << this->getPort() << std::endl;
+
             while(true) {
                 char buff[1024] = {0};
+                //Amount of received bytes
                 size_t bytes = serverSocket->receive_from(asio::buffer(buff), clientEndPoint);
                 if (bytes != 0) {
                     std::cout << buff << std::endl;
@@ -51,10 +62,8 @@ class SipServer {
                         return;
                     }
                 }
-
             }
         }
-
 };
 
 #endif //SIPSERVER_SIPSERVER_HPP
