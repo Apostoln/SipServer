f9c81d3534f2099391fd6bc25f86e63b69358973
Apostoln <nikbond97@gmail.com>
2017-06-19 13:49:18 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <asio.hpp>
+
+int main() {
+    return 0;
+}
---
+++ src/foo.hpp
@@ -0,0 +1,8 @@
+//
+// Created by portaone on 19.06.17.
+//
+
+#ifndef SIPSERVER_FOO_HPP
+#define SIPSERVER_FOO_HPP
+int foo() { return 0;}
+#endif //SIPSERVER_FOO_HPP
