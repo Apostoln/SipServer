b93b4679bf95b4430c0cd64b2a48d4e616a71f79
Apostoln <nikbond97@gmail.com>
2017-06-21 15:04:37 +0300

    Main function refactoring

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <SipServer.hpp>
 
 int main(int argc, char* argv[]) {
-    SipServer server;
-    if (argc > 1) {
+    SipServer* server;
+    if (argc == 1) {
         // Server port is 0 on default if other value is not specified on main function arguments.
         // If port value is 0, system set for socket any free and allowed port.
-        auto port = (unsigned short) std::atoi(argv[1]);
-        server.setPort(port);
+        server = new SipServer();
     }
-    server.run();
+    else {
+        // Create with specified port from main function arguments.
+        auto port = (unsigned short) std::atoi(argv[1]);
+        server = new SipServer(port);
+    }
+    server->run();
     return 0;
 }
