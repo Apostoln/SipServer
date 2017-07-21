d21959966b92156d07c9d18b595faf2b691392f3
Apostoln <nikbond@portaone.com>
2017-07-21 19:07:28 +0300

    Test small refactoring

--- test/main.py
+++ test/main.py
@@ -7,6 +7,22 @@ import time
 import platform
 import argparse
 
+def process(command, multiConnection = False):
+    result = []
+    commandResult = subprocess.Popen(command, stdout=subprocess.PIPE)
+    result.append(commandResult)
+
+    if not multiConnection:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        result.append(clientSocket)
+    else:
+        amountClients = 3
+        sockets = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(amountClients)]
+        result.append(sockets)
+
+    time.sleep(0.1)
+    return tuple(result)
+
 def test(testFunction):
     resultTest = testFunction()
     print("\n{0} is {1}passed\n".format(testFunction.__name__,'' if resultTest else 'NOT '))
@@ -15,34 +31,24 @@ def test(testFunction):
 def portListening():
     if DEBUG:
         print(sys._getframe().f_code.co_name)
-    command = [path, '-p', port]
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    time.sleep(0.1)
-    messages = ['Hello', 'World', 'q']
+    result, clientSocket = process([path, '-p', port])
     if DEBUG:
-        for m in messages:
+        for m in testMessages:
             print('< ', m)
-    for m in messages:
+    for m in testMessages:
         clientSocket.sendto(m.encode(), serverEndPoint)
     stdoutResult = [x.decode() for x in result.stdout]
     if DEBUG:
         for r in stdoutResult:
             print('> ', r[:-1])
-    if all(any(r.find(m) != -1 for r in stdoutResult) for m in messages):
-        return True
-    return False
+    return all(any(r.find(m) != -1 for r in stdoutResult) for m in testMessages)
 
 def portListeningMultiConnection():
     if DEBUG:
         print(sys._getframe().f_code.co_name)
-    command = [path, '-p', port]
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    time.sleep(0.1)
-    messages = ['Hello', 'World', 'q']
-    amountClients = 3
-    sockets = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(amountClients)]
-    for m in messages:
+    result, sockets = process([path, '-p', port], True)
+
+    for m in testMessages:
         for sock in sockets:
             if DEBUG:
                 print('< ', m)
@@ -51,9 +57,7 @@ def portListeningMultiConnection():
     if DEBUG:
         for r in stdoutResult:
             print('> ', r[:-1])
-    if all(all(any(r.find(m) != -1 for r in stdoutResult) for m in messages) for s in sockets):
-        return True
-    return False
+    return all(all(any(r.find(m) != -1 for r in stdoutResult) for m in testMessages) for s in sockets)
 
 def portListeningUsedPort():
     if DEBUG:
@@ -62,15 +66,11 @@ def portListeningUsedPort():
     usedSocket.sendto("q".encode(), serverEndPoint)
     usedPort = usedSocket.getsockname()[-1]
 
-    command = [path, '-p', str(usedPort)]
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    time.sleep(0.1)
-    messages = ['Hello', 'World', 'q']
+    result, clientSocket = process([path, '-p', str(usedPort)])
     if DEBUG:
-        for m in messages:
+        for m in testMessages:
             print('< ', m)
-    for m in messages:
+    for m in testMessages:
         clientSocket.sendto(m.encode(), serverEndPoint)
     return result.returncode != 0
 
@@ -82,10 +82,8 @@ def portListeningUnavailablePort():
         print("OS is", osType)
     if osType != 'Linux':
         return True
-    command = [path, '-p', '54']
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    time.sleep(0.1)
+
+    result, _ = process([path, '-p', '54'])
     return result.returncode != 0
 
 def portListeningSpecificInterface():
@@ -94,20 +92,15 @@ def portListeningSpecificInterface():
     networkInterface ='192.168.64.100'
     endPoint = (networkInterface, serverEndPoint[1])
     print(endPoint)
-    command = [path, '-p', port, '-n', networkInterface]
 
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    time.sleep(0.1)
-    messages = ['Hello', 'World', 'q']
+    result, clientSocket = process([path, '-p', port, '-n', networkInterface])
 
     if DEBUG:
-        for m in messages:
+        for m in testMessages:
             print('< ', m)
 
-    for m in messages:
+    for m in testMessages:
         clientSocket.sendto(m.encode(), endPoint)
-
 
     stdoutResult = [x.decode() for x in result.stdout]
 
@@ -115,56 +108,48 @@ def portListeningSpecificInterface():
         for r in stdoutResult:
             print('> ', r[:-1])
 
-    return all(any(r.find(m) != -1 for r in stdoutResult) for m in messages)
+    return all(any(r.find(m) != -1 for r in stdoutResult) for m in testMessages)
 
 def portListeningSpecificInterfaceUnavailable():
     if DEBUG:
         print(sys._getframe().f_code.co_name)
-    command = [path, '-p', '0', '-n', '1.2.3.4']
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    time.sleep(0.1)
+    result, _ = process([path, '-p', '0', '-n', '1.2.3.4'])
+
     return result.returncode != 0
 
 
 def echoServer():
     if DEBUG:
         print(sys._getframe().f_code.co_name)
-    command = [path, '-p', port]
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    time.sleep(0.1)
-    messages = ['Hello', 'World', 'q']
+    _, clientSocket = process([path, '-p', port])
+    
     if DEBUG:
-        for m in messages:
+        for m in testMessages:
             print('< ', m)
-    for m in messages:
+    for m in testMessages:
         clientSocket.sendto(m.encode(), serverEndPoint)
 
-    data = [clientSocket.recv(len(m)).decode() for m in messages]
+    data = [clientSocket.recv(len(m)).decode() for m in testMessages]
     if DEBUG:
         for d in data:
             print('> ', d)
-    return data == messages
+    return data == testMessages
 
 def echoMultiConnection():
     if DEBUG:
         print(sys._getframe().f_code.co_name)
-    command = [path, '-p', port]
-    result = subprocess.Popen(command, stdout=subprocess.PIPE)
-    time.sleep(0.1)
-    messages = ['Hello', 'World', 'q']
-    amountClients = 3
-    sockets = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(amountClients)]
-    for m in messages:
+    _, sockets = process([path, '-p', port], True)
+
+    for m in testMessages:
         for sock in sockets:
             if DEBUG:
                 print('< ', m)
             sock.sendto(m.encode(), serverEndPoint)
 
-    data = [[sock.recv(len(m)).decode() for m in messages] for sock in sockets]
+    data = [[sock.recv(len(m)).decode() for m in testMessages] for sock in sockets]
     if DEBUG:
         print("Data is", data)
-    return all(d == messages for d in data)
+    return all(d == testMessages for d in data)
 
 
 if __name__ == '__main__':
@@ -185,6 +170,9 @@ if __name__ == '__main__':
 
     serverEndPoint = (address, int(port))
     print(address, port, path)
+
+    testMessages = ['Hello', 'World', 'q']
+        
     test(portListening)
     test(portListeningMultiConnection)
     test(portListeningUnavailablePort)
