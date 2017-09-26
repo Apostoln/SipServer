0ea891d537aeec9dfb06d27803aaf45b15fc9abc
Apostoln <nikbond@portaone.com>
2017-09-26 18:27:21 +0300

    Emulation of logging hierarchy

--- src/utils.hpp
+++ src/utils.hpp
@@ -38,6 +38,28 @@ el::Level getLogLevel(std::string& logLevel) {
     }
 }
 
+void setLogLevel(el::Level level, el::Configurations& conf) {
+    //emulation of logging hierarchy because implementation in easylogging++ is too crutched
+
+    /* Logging hierarchy:
+     *      el::Level::Debug
+     *      el::Level::Info
+     *      el::Level::Warning
+     *      el::Level::Error
+     */
+
+    std::vector<el::Level> usedLevels = {el::Level::Debug, el::Level::Info, el::Level::Warning, el::Level::Error};
+
+    for(auto l: usedLevels) {
+        if (l != level) {
+            conf.set(l, el::ConfigurationType::Enabled, "false");
+        }
+        else {
+            break;
+        }
+    }
+}
+
 void configureLogger(bool isConsoleOut, std::string loggingFile, el::Level logLevel) {
     el::Configurations conf;
     conf.set(el::Level::Global, el::ConfigurationType::Format, "%level %datetime{%H:%m:%s} [%fbase]: %msg");
@@ -45,6 +67,7 @@ void configureLogger(bool isConsoleOut, std::string loggingFile, el::Level logLe
     if ("" != loggingFile) {
         conf.set(el::Level::Global, el::ConfigurationType::Filename, loggingFile);
     }
+    setLogLevel(logLevel, conf);
     el::Loggers::reconfigureAllLoggers(conf);
 
     el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
